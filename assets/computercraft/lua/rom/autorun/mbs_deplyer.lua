85335f0b938ac820da4bda76cccaafcbfe1a44a7
Lupus590 <lupussolitarius590@gmail.com>
2019-11-10 20:08:11 +0000

    change mbs_deplyer to mimic new MBS startup file

--- assets/computercraft/lua/rom/autorun/mbs_deplyer.lua
+++ assets/computercraft/lua/rom/autorun/mbs_deplyer.lua
@@ -1,10 +1,9 @@
 --  add mbs to startup
-
-if fs.exists("/startup/99_mbs.lua") then
+if fs.exists("/startup/00_mbs.lua") then
   return
 else
   fs.makeDir("/startup")
-  local f = fs.open("/startup/99_mbs.lua", "w")
+  local f = fs.open("/startup/00_mbs.lua", "w")
   f.writeLine([[
   shell.setAlias("mbs", "/rom/.mbs/mbs.lua")
   shell.run("/rom/.mbs/mbs.lua startup")
