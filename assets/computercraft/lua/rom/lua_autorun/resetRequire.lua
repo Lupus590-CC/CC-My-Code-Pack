3e7ee903cead0410f8917c44657087fe25693037
Lupus590 <lupussolitarius590@gmail.com>
2020-04-05 21:28:24 +0100

    stuff

---
+++ assets/computercraft/lua/rom/lua_autorun/resetRequire.lua
@@ -0,0 +1,22 @@
+
+local packagePreloadBackup = {}
+for k, v in pairs(package.preload) do
+  packagePreloadBackup[k] = v
+end
+local packageLoadedBackup = {}
+for k, v in pairs(package.loaded) do
+  packageLoadedBackup[k] = v
+end
+
+local function resetRequire()
+  package.preload = {}
+  for k, v in pairs(packagePreloadBackup) do
+    package.preload[k] = v
+  end
+  package.loaded = {}
+  for k, v in pairs(packageLoadedBackup) do
+    package.loaded[k] = v
+  end
+end
+
+return resetRequire
