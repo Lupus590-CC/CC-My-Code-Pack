c6c244c209d8caaabdcb17c66f48db42ee323b35
Lupus590 <lupussolitarius590@gmail.com>
2020-12-15 00:01:16 +0000

    should force update things I think

--- assets/computercraft/lua/rom/autorun/mbs_deployer.lua
+++ assets/computercraft/lua/rom/autorun/mbs_deployer.lua
@@ -1,9 +1,4 @@
---  add mbs to startup
-if fs.exists("/startup/00_mbs.lua") then
-  return
-else
-  fs.makeDir("/startup")
-  local f = fs.open("/startup/00_mbs.lua", "w")
-  f.writeLine("assert(loadfile(\"rom/.mbs/mbs.lua\", _ENV))('startup')")
-  f.close()
-end
+fs.makeDir("/startup")
+local f = fs.open("/startup/00_mbs.lua", "w")
+f.writeLine('shell.run("rom/.mbs/mbs.lua", "startup")')
+f.close()
